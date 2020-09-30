5285bcf62203e4259d8f751fe290ddf1bef77d81
MoonHKLee <mf311110@gmail.com>
2020-09-30 21:27:00 +0900

    auth deploy

--- src/main/java/kr/nutee/auth/Config/RedisConfig.java
+++ src/main/java/kr/nutee/auth/Config/RedisConfig.java
@@ -5,7 +5,6 @@ import kr.nutee.auth.Domain.Member;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -16,7 +15,7 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new JedisConnectionFactory();
+        return new LettuceConnectionFactory();
     }
 
     /*
